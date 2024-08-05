1adec178e29463ecde681e73d1a470eddeecdc77
David Chamling Rai <DavidRai441@gmail.com>
2024-08-05 22:42:01 +0545

    .env file and useCustomForm added

---
+++ src/hooks/useCustomForm.ts
@@ -0,0 +1,108 @@
+import { useForm, FieldValues, Path } from 'react-hook-form'
+import { toast } from 'sonner'
+
+type RequestOptions = {
+	method?: string
+	headers?: HeadersInit
+	body?: string
+}
+
+type ErrorResponse = {
+	status: number
+	message: string
+	errors: { [key: string]: string }
+}
+
+export type ApiResponse<T> = {
+	data: T
+	statusCode: number
+	message: string
+	success: boolean
+}
+
+export const useCustomForm = <T extends FieldValues>(options = {}) => {
+	const form = useForm<T>({
+		mode: 'onTouched',
+		...options,
+	})
+	const { handleSubmit, register, control, formState, setError } = form
+	const { errors, isDirty, isSubmitting } = formState
+
+	const errorMessages: { [key: number]: string } = {
+		401: 'Unauthorized access. Please log in.',
+		403: 'Forbidden access. You do not have the required permissions.',
+		404: 'Resource not found.',
+		500: 'Internal server error. Please try again later.',
+	}
+
+	const handleErrorResponse = async (
+		response: Response,
+		data: ErrorResponse
+	) => {
+		if (data.message) {
+			throw new Error(data.message)
+		}
+
+		let errorMessage = 'Something went wrong'
+
+		if (errorMessages[response.status]) {
+			errorMessage = errorMessages[response.status]
+		} else if (response.status === 422) {
+			errorMessage = data.message
+			Object.keys(data.errors).forEach((key: string) => {
+				setError(key as Path<T>, {
+					type: 'custom',
+					message: data.errors[key],
+				})
+			})
+		} else {
+			errorMessage = data.message
+		}
+		throw new Error(errorMessage)
+	}
+
+	const api = async <R>(url: string, options: RequestOptions = {}) => {
+		options.headers = {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${localStorage.getItem('token') ?? ''}`,
+			...options.headers,
+		}
+
+		try {
+			const response = await fetch(
+				`${import.meta.env.VITE_BASE_URL}/${url}`,
+				options
+			)
+			const data = await response.json()
+
+			if (!response.ok) {
+				await handleErrorResponse(response, data)
+			}
+
+			const contentType = response.headers.get('Content-Type')
+			if (!contentType || !contentType.includes('application/json')) {
+				throw new Error('Invalid response format')
+			}
+
+			return data as ApiResponse<R>
+		} catch (error) {
+			if (error instanceof Error) {
+				toast.error(error.message)
+			} else {
+				toast.error('Cannot connect to server: An unknown error occurred')
+			}
+			throw error
+		}
+	}
+
+	return {
+		api,
+		handleSubmit,
+		register,
+		control,
+		errors,
+		isDirty,
+		isSubmitting,
+	}
+}
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,28 +1,61 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { useCustomForm } from '@/hooks/useCustomForm'
 import { Checkbox } from '@radix-ui/react-checkbox'
 import { Label } from '@radix-ui/react-label'
+import { z } from 'zod'
+import { DevTool } from '@hookform/devtools'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const LoginSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(8),
+})
+
+type LoginFormValue = z.infer<typeof LoginSchema>
 
 const LoginPage = () => {
+	const { api, register, handleSubmit, errors, control } =
+		useCustomForm<LoginFormValue>({
+			resolver: zodResolver(LoginSchema),
+		})
+
+	const onSubmit = async (data: LoginFormValue) => {
+		const response = await api('api/login', {
+			method: 'POST',
+			body: JSON.stringify(data),
+		})
+		console.log(response)
+	}
 	return (
 		<>
 			<div className='mt-20 mb-20'>
-				<form className='max-w-md mx-auto p-4 bg-white border border-gray-200'>
+				<form
+					className='max-w-md mx-auto p-4 bg-white border border-gray-200'
+					onSubmit={handleSubmit(onSubmit)}
+					noValidate
+				>
 					<h2 className='text-2xl mb-8 font-semibold text-center'>
 						Please login
 					</h2>
 					<Input
 						type='email'
 						placeholder='Email'
+						{...register('email')}
+						error={errors.email}
 					/>
 					<Input
 						type='password'
 						placeholder='Password'
+						{...register('password')}
+						error={errors.password}
 					/>
 					<Checkbox id='remember-me' />
 					<Label htmlFor='remember-me'>Remember me</Label>
 					<Button className='w-full'>Login</Button>
 				</form>
+
+				<DevTool control={control} />
 			</div>
 		</>
 	)
