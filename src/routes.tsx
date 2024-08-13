250eef9a3026cef234e0a3760c464985268b870e
David Chamling Rai <DavidRai441@gmail.com>
2024-08-14 02:28:16 +0545

    home page added

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import UserListPage from './pages/users/UserListPage'
+import IndexPage from './pages/IndexPage'
 
 export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <IndexPage />,
+	},
 	{
 		path: '/login',
 		element: <LoginPage />,
