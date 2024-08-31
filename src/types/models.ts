395a1c6620ec30e59641491cceec470e2be4aeae
David Chamling Rai <DavidRai441@gmail.com>
2024-09-01 02:08:46 +0545

    kotha detail page additional info added

--- src/types/models.ts
+++ src/types/models.ts
@@ -21,3 +21,40 @@ export type Kotha = {
 	purpose: string
 	created_at: Date
 }
+
+export type KothaDetail = {
+	id: number
+	title: string
+	description: string
+	images: Image[]
+	category_id: number
+	category: Category
+	price: number
+	negotiable: number
+	purpose: string
+	facility: Facility
+	contact: Contact
+	created_at: Date
+}
+
+export type Contact = {
+	id: number
+	kotha_id: number
+	number: string
+	alternative_number: string
+	longitude: string
+	latitude: string
+	created_at: Date
+	updated_at: Date
+}
+
+export type Facility = {
+	bed_room: number
+	kitchen: string
+	bathroom: number
+	parking: string
+	balcony: string
+	rental_floor_id: number
+	rental_floor_name: string
+	water_facility: string
+}
